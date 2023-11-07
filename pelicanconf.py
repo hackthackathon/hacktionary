4a1b7f1dedc388c85d5cdf715ce24a820e94e785
Erik Tollerud <erik.tollerud@gmail.com>
2023-11-07 11:43:55 +0100

    add license

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,5 +27,8 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = False
 
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+ARTICLE_ORDER_BY = 'basename'
+
+RELATIVE_URLS = True
+
+CC_LICENSE = 'CC-BY'
